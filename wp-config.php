2fc4a822d3e9b470b7eef49e24c9d31e29cc0ef0
max-ma1 <max.ma@live.ca>
2018-08-14 16:14:21 -0400

    fix

--- wp-config.php
+++ wp-config.php
@@ -82,7 +82,7 @@ define('WP_ALLOW_MULTISITE', true);
 
 define('MULTISITE', true);
 define('SUBDOMAIN_INSTALL', false);
-define('DOMAIN_CURRENT_SITE', 'www.mahjong-ca.org');
+define('DOMAIN_CURRENT_SITE', 'www.mahjong-ca.azurewebsites.net');
 define('PATH_CURRENT_SITE', '/');
 define('SITE_ID_CURRENT_SITE', 1);
 define('BLOG_ID_CURRENT_SITE', 6);
